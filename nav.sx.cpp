7b0dcaf8364cd12067275ab2df36b72e76424bf7
Denis <carriere.denis@gmail.com>
2020-10-23 12:14:04 -0400

    add out

--- nav.sx.cpp
+++ nav.sx.cpp
@@ -14,15 +14,17 @@ void sx::nav::on_transfer( const name from, const name to, const asset quantity,
     // ignore transfers
     if ( to != get_self() || memo == get_self().to_string() || from == "eosio.ram"_n) return;
 
-    // only accept USDT
+    // only accepts USDT
     eosio::check( get_first_receiver() == "tethertether"_n && quantity.symbol == symbol{"USDT", 4}, "only 4,USDT@tethertether is accepted");
+    eosio::check( quantity >= asset{1000, symbol{"USDT", 4}}, "transfer must exceed 0.1000 USDT");
 
     // issue SX @ 1:1 ratio
     eosio::token::issue_action issue( "token.sx"_n, { "token.sx"_n, "active"_n });
     eosio::token::transfer_action transfer( "token.sx"_n, { get_self(), "active"_n });
 
     // issue 2x (1x => from, 1x => nav.sx)
-    issue.send( "token.sx"_n, quantity * 2, "nav" );
-    transfer.send( "token.sx"_n, get_self(), quantity * 2, "nav" );
-    transfer.send( get_self(), from, quantity, "nav" );
+    const asset out = asset{quantity.amount, symbol{"SX", 4}};
+    issue.send( "token.sx"_n, out * 2, "nav" );
+    transfer.send( "token.sx"_n, get_self(), out * 2, "nav" );
+    transfer.send( get_self(), from, out, "nav" );
 }
